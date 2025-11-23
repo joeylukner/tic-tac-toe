c9b7bb53b8f9eba78bf00cce85921ec1d5d1dee2
Joey Lukner <jlukner1@swarthmore.edu>
2025-11-22 18:49:52 -0600

    Render squares with loops instead of hardcoding

--- src/src/App.js
+++ src/src/App.js
@@ -31,21 +31,16 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {[...Array(3)].map((_, row) => (
+        <div className="board-row">
+          {[...Array(3)].map((_, col) => (
+            <Square
+              value={squares[row * 3 + col]}
+              onSquareClick={() => handleClick(row * 3 + col)}
+            />
+          ))}
+        </div>
+      ))}
     </>
   );
 }
